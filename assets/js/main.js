502f9e4f07ed5953b9ce1f86199c8e488e6f1ddc
catmakeswork <thecatmakes@gmail.com>
2023-02-27 23:43:59 -0800

    REAL EYES!!!!!!!!!

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,31 +12,6 @@
 		$body = $('body'),
 		$sidebar = $('#sidebar');
 
-		var eye = $(".eye");
-
-		eye.css({
-			'-webkit-transform': 'rotate(' + -90 + 'deg)',
-			'-moz-transform': 'rotate(' + -90 + 'deg)',
-			'-ms-transform': 'rotate(' + -90 + 'deg)',
-			'transform': 'rotate(' + -90 + 'deg)'
-		});
-
-
-		$('body').mousemove(function (event) {
-			console.log('eye', eye)
-			var x = (eye.offset().left) + (eye.width() / 2);
-			var y = (eye.offset().top) + (eye.height() / 2);
-			var rad = Math.atan2(event.pageX - x, event.pageY - y);
-			var rot = (rad * (180 / Math.PI) * -1) + 180;
-			eye.css({
-				'-webkit-transform': 'rotate(' + rot + 'deg)',
-				'-moz-transform': 'rotate(' + rot + 'deg)',
-				'-ms-transform': 'rotate(' + rot + 'deg)',
-				'transform': 'rotate(' + rot + 'deg)'
-			});
-		});
-
-
 	// Nav.
 
 	// Title Bar.
